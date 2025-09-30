b7a290fd690b8a28db5850d5bccfc992e50ac004
IrhamIzza <Irhamizza21@gmail.com>
2025-09-30 08:44:06 +0700

    add home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
@@ -7,10 +6,10 @@ function App() {
   return (
     <>
     <Navbar>
+    </Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </Navbar>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
+import { use, useEffect, useState } from "react";   
 export default function Home() {
+    const [anime, setAnime] = useState([]);
+    const [anime2, setAnime2] = useState([]);
+
+    async function getData() {
+        const response = await fetch("https://api.jikan.moe/v4/anime/top/tv?limit=10"); 
+        const data = await response.json();
+        setAnime(data.data);
+    }
+
+    async function getData2() {
+        const response = await fetch("https://api.jikan.moe/v4/anime/top/movie?limit=10"); 
+        const data = await response.json();        
+    }
+
+    useEffect(() => {
+        getData();
+        getData2();
+    }, []);
+
     return (
         <>
-            <h1>Home</h1>
+            <h1 className="">Home</h1>
         </>
     )
 }
