ca22fc9806f9d9efb1074f2e0ad7e3925f12c7be
IrhamIzza <Irhamizza21@gmail.com>
2026-01-08 09:52:31 +0700

    fix loading

--- src/pages/Anime.jsx
+++ src/pages/Anime.jsx
@@ -60,16 +60,20 @@ export default function Anime() {
     getData();
   }, []);
 
+  if (loading || !anime) {
+    return (
+    <div className="bg-gray-900 text-white md:px-20 pt-2 pb-5">
+      <div className="flex justify-center items-center h-screen">
+        <i className="ph ph-circle-notch animate-spin text-6xl text-white"></i>
+      </div>
+    </div>
+    );
+  }
+
   return (
     <>
       {/* Loader */}
-
-      <div className="bg-gray-900 text-white md:px-20 pt-2">
-        {loading && (
-          <div className="flex justify-center items-center h-screen">
-            <i className="ph ph-circle-notch animate-spin text-6xl text-white"></i>
-          </div>
-        )}
+      <div className="bg-gray-900 text-white md:px-20 pt-2 pb-5">
 
         {/*Search Bar */}
         <form
@@ -110,10 +114,7 @@ export default function Anime() {
 
         {/* Hasil Search */}
         {searchResult.length > 0 && (
-          <section className="px-6 mb-12">
-            <h3 className="text-2xl font-semibold mb-6">
-              🔎 Search Results for "{search}"
-            </h3>
+          <section className="px-6 pb-10 py-6">
             {/* loading */}
             {searching ? (
               <div className="flex justify-center items-center py-10">
@@ -142,92 +143,100 @@ export default function Anime() {
           </section>
         )}
 
-        {/* Top Anime */}
-        <section className="px-6 py-10">
-          <div className="flex justify-between items-center">
-            <h3 className="text-2xl font-semibold mb-6">🔥 Top Anime</h3>
-            <Link to="/anime/trending">
-              <h3 className="text-md text-gray-400 font-semibold mb-6 hover:text-white transition">
-                View All
-              </h3>
-            </Link>
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-6">
-            {anime.map((item) => (
-              <div className="bg-gray-800 rounded-xl shadow-lg overflow-hidden hover:scale-105 transition">
-                <Link to={`/anime/detail/${item.mal_id}`} key={item.mal_id}>
-                  <img
-                    src={item.images.jpg.image_url}
-                    alt={item.title}
-                    className="w-full h-64 object-cover"
-                  />
-                  <div className="p-4">
-                    <h4 className="text-lg font-bold">{item.title}</h4>
-                    <p className="text-sm text-gray-400">{item.status}</p>
-                  </div>
+        {searchResult.length === 0 && (
+          <div>
+            {/* Top Anime */}
+            <section className="px-6 pb-10 py-6">
+              <div className="flex justify-between items-center">
+                <h3 className="text-2xl font-semibold mb-6">🔥 Top Anime</h3>
+                <Link to="/anime/trending">
+                  <h3 className="text-md text-gray-400 font-semibold mb-6 hover:text-white transition">
+                    View All
+                  </h3>
                 </Link>
               </div>
-            ))}
-          </div>
-        </section>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-6">
+                {anime.map((item) => (
+                  <div className="bg-gray-800 rounded-xl shadow-lg overflow-hidden hover:scale-105 transition">
+                    <Link to={`/anime/detail/${item.mal_id}`} key={item.mal_id}>
+                      <img
+                        src={item.images.jpg.image_url}
+                        alt={item.title}
+                        className="w-full h-64 object-cover"
+                      />
+                      <div className="p-4">
+                        <h4 className="text-lg font-bold">{item.title}</h4>
+                        <p className="text-sm text-gray-400">{item.status}</p>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </section>
 
-        {/* Popular Anime */}
-        <section className="px-6 py-10">
-          <div className="flex justify-between items-center">
-            <h3 className="text-2xl font-semibold mb-6">🔥 Popular Anime</h3>
-            <Link to="/anime/popular">
-              <h3 className="text-md text-gray-400 font-semibold mb-6 hover:text-white transition">
-                View All
-              </h3>
-            </Link>
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-6">
-            {anime2.map((item) => (
-              <div className="bg-gray-800 rounded-xl shadow-lg overflow-hidden hover:scale-105 transition">
-                <Link to={`/anime/detail/${item.mal_id}`} key={item.mal_id}>
-                  <img
-                    src={item.images.jpg.image_url}
-                    alt={item.title}
-                    className="w-full h-64 object-cover"
-                  />
-                  <div className="p-4">
-                    <h4 className="text-lg font-bold">{item.title}</h4>
-                    <p className="text-sm text-gray-400">{item.status}</p>
-                  </div>
+            {/* Popular Anime */}
+            <section className="px-6 py-10">
+              <div className="flex justify-between items-center">
+                <h3 className="text-2xl font-semibold mb-6">
+                  🔥 Popular Anime
+                </h3>
+                <Link to="/anime/popular">
+                  <h3 className="text-md text-gray-400 font-semibold mb-6 hover:text-white transition">
+                    View All
+                  </h3>
                 </Link>
               </div>
-            ))}
-          </div>
-        </section>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-6">
+                {anime2.map((item) => (
+                  <div className="bg-gray-800 rounded-xl shadow-lg overflow-hidden hover:scale-105 transition">
+                    <Link to={`/anime/detail/${item.mal_id}`} key={item.mal_id}>
+                      <img
+                        src={item.images.jpg.image_url}
+                        alt={item.title}
+                        className="w-full h-64 object-cover"
+                      />
+                      <div className="p-4">
+                        <h4 className="text-lg font-bold">{item.title}</h4>
+                        <p className="text-sm text-gray-400">{item.status}</p>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </section>
 
-        {/* Upcoming Anime */}
-        <section className="px-6 py-10">
-          <div className="flex justify-between items-center">
-            <h3 className="text-2xl font-semibold mb-6">🔥 Upcoming Anime</h3>
-            <Link to="/anime/upcoming">
-              <h3 className="text-md text-gray-400 font-semibold mb-6 hover:text-white transition">
-                View All
-              </h3>
-            </Link>
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-6">
-            {anime3.map((item) => (
-              <div className="bg-gray-800 rounded-xl shadow-lg overflow-hidden hover:scale-105 transition">
-                <Link to={`/anime/detail/${item.mal_id}`} key={item.mal_id}>
-                  <img
-                    src={item.images.jpg.image_url}
-                    alt={item.title}
-                    className="w-full h-64 object-cover"
-                  />
-                  <div className="p-4">
-                    <h4 className="text-lg font-bold">{item.title}</h4>
-                    <p className="text-sm text-gray-400">{item.status}</p>
-                  </div>
+            {/* Upcoming Anime */}
+            <section className="px-6 py-10">
+              <div className="flex justify-between items-center">
+                <h3 className="text-2xl font-semibold mb-6">
+                  🔥 Upcoming Anime
+                </h3>
+                <Link to="/anime/upcoming">
+                  <h3 className="text-md text-gray-400 font-semibold mb-6 hover:text-white transition">
+                    View All
+                  </h3>
                 </Link>
               </div>
-            ))}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-6">
+                {anime3.map((item) => (
+                  <div className="bg-gray-800 rounded-xl shadow-lg overflow-hidden hover:scale-105 transition">
+                    <Link to={`/anime/detail/${item.mal_id}`} key={item.mal_id}>
+                      <img
+                        src={item.images.jpg.image_url}
+                        alt={item.title}
+                        className="w-full h-64 object-cover"
+                      />
+                      <div className="p-4">
+                        <h4 className="text-lg font-bold">{item.title}</h4>
+                        <p className="text-sm text-gray-400">{item.status}</p>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </section>
           </div>
-        </section>
+        )}
       </div>
     </>
   );
--- src/pages/DetailAnime.jsx
+++ src/pages/DetailAnime.jsx
@@ -41,75 +41,79 @@ export default function DetailAnime() {
   //   if (loading) return <Loader fullscreen />;
   //   if (!anime) return <div className="text-white">Anime not found.</div>;
 
+  if (loading || !anime) {
+    return (
+    <div className="bg-gray-900 text-white md:px-20 pt-2 pb-5">
+      <div className="flex justify-center items-center h-screen">
+        <i className="ph ph-circle-notch animate-spin text-6xl text-white"></i>
+      </div>
+    </div>
+    );
+  }
+
   return (
     <div className="bg-gray-900 text-white min-h-screen px-6 md:px-20 py-10">
-      {loading && (
-        <div className="flex justify-center items-center h-screen">
-          <i className="ph ph-circle-notch animate-spin text-6xl text-white"></i>
+      <Link to="/anime" className="text-gray-400 hover:text-white mb-4 inline-block">← Back</Link>
+      <div className="pb-5">
+        <div className="flex flex-col md:flex-row gap-5 items-start">
+          <img
+            src={anime.images?.webp.image_url}
+            alt={anime.title}
+            className="rounded-lg shadow-lg w-56"
+          // loading="lazy"
+          />
+
+          <div className="flex-1 ">
+            <h1 className="text-3xl font-bold mb-4">{anime.title}</h1>
+            <p className="text-gray-400  mb-3 italic">{anime.title_japanese}</p>
+            <p className="mb-4 text-sm text-gray-300 leading-relaxed">{anime.synopsis}</p>
+
+
+          </div>
         </div>
-      )}
-      {!loading && (
-        <>
-          <Link to="/anime" className="text-gray-400 hover:text-white mb-4 inline-block">← Back</Link>
-          <div className="pb-5">
-            <div className="flex flex-col md:flex-row gap-5 items-start">
-              <img
-                src={anime.images.webp.image_url}
-                alt={anime.title}
-                className="rounded-lg shadow-lg w-56"
-              />
+      </div>
+      {/* RANK */}
+      <div className="bg-gray-800 w-56 rounded-sm p-2">
+        <p className="text-center text-blue-300 "> RANK : {anime.rank}</p>
+      </div>
 
-              <div className="flex-1 ">
-                <h1 className="text-3xl font-bold mb-4">{anime.title}</h1>
-                <p className="text-gray-400  mb-3 italic">{anime.title_japanese}</p>
-                <p className="mb-4 text-sm text-gray-300 leading-relaxed">{anime.synopsis}</p>
+      <div className="flex flex-1 py-3 gap-5">
+        <div className="bg-gray-800 p-4 rounded-sm w-56">
+          <ul className="text-sm text-gray-300 space-y-1">
+            <li><strong>Type:</strong> {anime.type}</li>
+            <li><strong>Episodes:</strong> {anime.episodes || "?"}</li>
+            <li><strong>Status:</strong> {anime.status}</li>
+            <li><strong>Score:</strong> ⭐ {anime.score || "N/A"}</li>
 
-
-              </div>
-            </div>
-          </div>
-          {/* RANK */}
-          <div className="bg-gray-800 w-56 rounded-sm p-2">
-            <p className="text-center text-blue-300 "> RANK : {anime.rank}</p>
-          </div>
-
-          <div className="flex flex-1 py-3 gap-5">
-            <div className="bg-gray-800 p-4 rounded-sm w-56">
-              <ul className="text-sm text-gray-300 space-y-1">
-                <li><strong>Type:</strong> {anime.type}</li>
-                <li><strong>Episodes:</strong> {anime.episodes || "?"}</li>
-                <li><strong>Status:</strong> {anime.status}</li>
-                <li><strong>Score:</strong> ⭐ {anime.score || "N/A"}</li>
-
-              </ul>
-            </div>
-            <div className="flex flex-1 flex-col gap-1">
-              <div className="text-gray-300 ">Characters</div>
-              <div>
-                <div className="flex flex-wrap gap-2">
-                {characters.map((item) => (
-                  <div key={item.character.mal_id} className="flex card justify-between bg-gray-800 mb-2 w-md">
-                    <div className="flex">
-                      <img className="w-20" src={item.character.images.webp.image_url} alt="" />
-                      <div className="p-2">
-                        <h4 className="text-md text-gray-300 font-bold">{item.character.name}</h4>
-                        <p className="text-xs text-gray-400">{item.role}</p>
-                      </div>
-                    </div>
-                    <div className="flex ">
-                      <div className="p-2 ">
-                        <h4 className="text-md text-gray-300 font-bold">{item.voice_actors[0]?.person.name}</h4>
-                        <p className="text-xs text-gray-400">Main</p>
-                      </div>
-                      <img className="w-20" src={item.voice_actors[0]?.person.images.jpg.image_url} alt="" />
+          </ul>
+        </div>
+        <div className="flex flex-1 flex-col gap-1">
+          <div className="text-gray-300 ">Characters</div>
+          <div>
+            <div className="flex flex-wrap gap-3">
+              {characters.slice(0, 6).map((item) => (
+                <div key={item.character.mal_id} className="flex card justify-between bg-gray-800 w-md">
+                  <div className="flex">
+                    <img className="w-20" src={item.character.images.webp.image_url} alt="" />
+                    <div className="p-2">
+                      <h4 className="text-md text-gray-300 font-bold">{item.character.name}</h4>
+                      <p className="text-xs text-gray-400">{item.role}</p>
                     </div>
                   </div>
-                ))}
+                  <div className="flex ">
+                    <div className="p-2 ">
+                      <h4 className="text-md text-gray-300 font-bold">{item.voice_actors[0]?.person.name}</h4>
+                      <p className="text-xs text-gray-400">{item.voice_actors[0]?.language}</p>
+                    </div>
+                    <img className="w-20" src={item.voice_actors[0]?.person.images.jpg.image_url} alt="" />
+                  </div>
                 </div>
-              </div>
+              ))}
             </div>
           </div>
-          {/* <div className="flex flex-col md:flex-row gap-8">
+        </div>
+      </div>
+      {/* <div className="flex flex-col md:flex-row gap-8">
 
             <img src={anime.images.webp.image_url} alt={anime.title} className="w-full md:w-1/3 rounded-lg shadow-lg" />
 
@@ -127,8 +131,6 @@ export default function DetailAnime() {
               </ul>
             </div>
           </div> */}
-        </>
-      )}
     </div>
   );
 }
